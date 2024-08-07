4e4fad48eeddc9957168bd741d9af2353b1785d5
Pankaj Kumar <pankajrwr@gmail.com>
2024-08-08 01:17:23 +0530

    initialise tests structure

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,7 +1,32 @@
-const stringCalculator = require('./calculator');
+const stringCalculator = require("./calculator");
 
-describe('Calculator Module', () => {
-test('something to test', () => {
-  throw new Error('not matched');
-});
+describe("Calculator Module", () => {
+  test("it should return 0 when empty string is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should return the number itself when a single number as string is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should return the sum of the numbers when a string of two comma separated numbers is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should return the sum of all the numbers when a string of any amount of comma separated numbers is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should return sum of all the numbers when string of numbers separated by \n is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should return sum of all the numbers when a string of numbers separated by specified delimeter is passed", () => {
+    throw new Error("failed");
+  });
+
+  test("it should throw exception if a string containing negative numbers is passed", () => {
+    throw new Error("failed");
+  });
+
 });
