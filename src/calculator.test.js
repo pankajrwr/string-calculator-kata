d05fd5941abdb6b8e88696ab0e84d16e88897867
Pankaj Kumar <pankajrwr@gmail.com>
2024-08-08 01:32:30 +0530

    implement test definitions

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,31 +2,30 @@ const stringCalculator = require("./calculator");
 
 describe("Calculator Module", () => {
   test("it should return 0 when empty string is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("")).toBe(0);
   });
 
   test("it should return the number itself when a single number as string is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("1")).toBe(1);
   });
 
   test("it should return the sum of the numbers when a string of two comma separated numbers is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("1,5")).toBe(6);
   });
 
   test("it should return the sum of all the numbers when a string of any amount of comma separated numbers is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("1,5,4,2,3,1,7")).toBe(1 + 5 + 4 + 2 + 3 + 1 + 7);
   });
 
   test("it should return sum of all the numbers when string of numbers separated by \n is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("1\n2,3")).toBe(6);
   });
 
   test("it should return sum of all the numbers when a string of numbers separated by specified delimeter is passed", () => {
-    throw new Error("failed");
+    expect(stringCalculator("//;\n1;2")).toBe(3);
   });
 
-  test("it should throw exception if a string containing negative numbers is passed", () => {
-    throw new Error("failed");
+  test("it should throw exception displaying list of negative numbers when a string containing negative numbers is passed", () => {
+    expect(stringCalculator("1,-2")).toBe("negative numbers not allowed -2");
   });
-
 });
