a61c15ef857fef58aa93987860c9007c4b637f5b
Pankaj Kumar <pankajrwr@gmail.com>
2024-08-08 02:27:02 +0530

    refactor the code to generalize and to remove repetitive code

--- src/calculator.js
+++ src/calculator.js
@@ -3,17 +3,23 @@ function stringCalculator(input) {
     return 0;
   }
 
+  let delimeter;
   if (input.startsWith("//")) {
-    const delimeter = input.charAt(2);
+    /**
+     * if the input starts with // it means here we have special delimeter surrounded by // and \n.
+     * Because this whole pattern will be at the start of the string, the char at index 2 will give us the delimeter
+     * And the remaining part of the input after the pattern will give us our actual input we need to operate on
+     */
+    delimeter = input.charAt(2);
     input = input.substring(input.indexOf("\n") + 1);
-    const inputToArray = input.split(delimeter);
-    return inputToArray.reduce(
-      (sum, currentValue) => sum + parseInt(currentValue),
-      0
-    );
+  } else {
+    // By default we will consider comma as our delimeter
+    delimeter = ",";
+    // We will replace \n with comma just in case it exists in the input
+    input = input.replace("\n", ",");
   }
 
-  const numberArray = input.replace("\n", ",").split(",");
+  const numberArray = input.split(delimeter);
 
   const sum = numberArray.reduce(
     (sum, currentValue) => sum + parseInt(currentValue),
