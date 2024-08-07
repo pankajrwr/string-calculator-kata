8e3de53d5c4a806d69b56522ae98e3fb393e3448
Pankaj Kumar <pankajrwr@gmail.com>
2024-08-08 01:56:06 +0530

    handle comma separated numbers string input

--- src/calculator.js
+++ src/calculator.js
@@ -8,6 +8,13 @@ function stringCalculator(input) {
   if (numberArray.length == 1) {
     return parseInt(input);
   }
+
+  const sum = numberArray.reduce(
+    (sum, currentValue) => sum + parseInt(currentValue),
+    0
+  );
+
+  return sum;
 }
 
 module.exports = stringCalculator;
