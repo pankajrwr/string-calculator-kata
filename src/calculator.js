f7e753c1327a75fa2befc09a223f31581d08c011
Pankaj Kumar <pankajrwr@gmail.com>
2024-08-08 02:15:20 +0530

    handle custom delimeter separated numbers string input

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,17 @@ function stringCalculator(input) {
     return 0;
   }
 
-  const numberArray = input.replace('\n', ',').split(",");
+  if (input.startsWith("//")) {
+    const delimeter = input.charAt(2);
+    input = input.substring(input.indexOf("\n") + 1);
+    const inputToArray = input.split(delimeter);
+    return inputToArray.reduce(
+      (sum, currentValue) => sum + parseInt(currentValue),
+      0
+    );
+  }
+
+  const numberArray = input.replace("\n", ",").split(",");
 
   const sum = numberArray.reduce(
     (sum, currentValue) => sum + parseInt(currentValue),
